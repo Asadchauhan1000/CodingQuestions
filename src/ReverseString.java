ed933fc5dc5d824b22517cdcc47a6b32102994fc
asadchauhan1000 <asadchauhan1000@gmail.com>
2026-02-04 20:46:31 +0530

    feat: add reversestring program

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+public class ReverseString {
+public static String reverse(String input) {
+    if (input == null) {
+        return null;
+    }
+    StringBuilder reversed = new StringBuilder();
+    for (int i = input.length() - 1; i >= 0; i--) {
+        reversed.append(input.charAt(i));
+    }
+    return reversed.toString();
+}
+
+public static void main(String[] args) {
+    String original = "Hello, World!";
+    String reversed = reverse(original);
+    System.out.println("Original: " + original);
+    System.out.println("Reversed: " + reversed);
+}
+}
